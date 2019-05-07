8957f1e2fbae623eb74599f0606f31c27b453a5f
Francesco Ghianda <francy9661@gmail.com>
2019-05-07 22:26:45 +0200

    test

---
+++ src/main/java/it/polimi/se2019/utils/timer/NegativeTimeException.java
@@ -0,0 +1,9 @@
+package it.polimi.se2019.utils.timer;
+
+public class NegativeTimeException extends RuntimeException
+{
+    public NegativeTimeException()
+    {
+        super();
+    }
+}
---
+++ src/test/java/it/polimi/se2019/TimerTest.java
@@ -0,0 +1,62 @@
+package it.polimi.se2019;
+
+import it.polimi.se2019.utils.timer.AlreadyExistingTimerException;
+import it.polimi.se2019.utils.timer.NegativeTimeException;
+import it.polimi.se2019.utils.timer.Timer;
+
+import org.junit.Test;
+import static junit.framework.TestCase.fail;
+
+public class TimerTest
+{
+
+    @Test
+    public void timerCreation()
+    {
+        Timer t = null;
+        try
+        {
+            t = Timer.createTimer("timer1", 10);
+        }
+        catch (AlreadyExistingTimerException | NegativeTimeException e)
+        {
+            fail();
+        }
+
+        Timer.destroyTimer("timer1");
+        assert true;
+    }
+
+    @Test
+    public void negativeTimeTest()
+    {
+        Timer timer = null;
+        try
+        {
+            timer = Timer.createTimer("timer1", -1);
+            fail();
+        }
+        catch (NegativeTimeException e)
+        {
+            assert true;
+        }
+    }
+
+    @Test
+    public void alreadyExistingTimerExceptionTest()
+    {
+        Timer t1 = null;
+        Timer t2 = null;
+        try
+        {
+            t1 = Timer.createTimer("timer1", 20);
+            t2 = Timer.createTimer("timer1", 10);
+            fail();
+        }
+        catch (AlreadyExistingTimerException e)
+        {
+            Timer.destroyTimer("timer1");
+            assert true;
+        }
+    }
+}
