0df4bb374ce4b412e5e0720b14ce5d488ef3950d
Simone Grandi <simone.grandi@mail.polimi.it>
2019-05-26 22:17:15 +0200

    Player changes.

--- src/main/java/it/polimi/se2019/player/NegativeValueException.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.se2019.player;
-
-public class NegativeValueException extends RuntimeException
-{
-    public NegativeValueException()
-    {
-        super();
-    }
-}
--- src/main/java/it/polimi/se2019/player/TooHighValueException.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.se2019.player;
-
-public class TooHighValueException extends RuntimeException
-{
-    public TooHighValueException()
-    {
-        super();
-    }
-}
